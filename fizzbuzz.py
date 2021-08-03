0cb032dd024139f61ebfdcb5f7f242332f0806db
markovicnevena703 <87694299+markovicnevena703@users.noreply.github.com>
2021-08-03 17:58:21 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+i = 1
+while (i <=1000):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    elif (i % 3 == 0):
+        print("Fizz")
+    else: 
+        print(i)
+    i+=1
